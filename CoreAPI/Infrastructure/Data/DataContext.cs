7ae657677913964b0268408203d141508dedbd0b
samir <samalk1106@gmail.com>
2020-06-10 21:21:55 +0530

    Code refractor

---
+++ CoreAPI/Infrastructure/Data/DataContext.cs
@@ -0,0 +1,13 @@
+
+using Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options){}
+        public DbSet<Product> Products {get;set;}
+       
+    }
+}
